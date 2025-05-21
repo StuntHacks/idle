bc6658e39c9e9ee5a973dc735d62e958c07c93e4
StuntHacks <me@stunthacks.eu>
2025-05-22 00:51:58 +0200

    Add icons, preprocess svgs

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,27 @@
 const gulp = require("gulp");
 const fileInclude = require("gulp-file-include");
+const through2 = require("through2");
+const path = require("path");
+const fs = require("fs");
+
+gulp.task("preprocess-svgs", function () {
+  return gulp
+    .src("src/assets/icons/*.svg")
+    .pipe(
+      through2.obj(function (file, _, cb) {
+        if (file.isBuffer()) {
+          const contents = file.contents.toString();
+          const modified = contents.replace(
+            ' style="height: 512px; width: 512px;"',
+            ""
+          );
+          file.contents = Buffer.from(modified);
+        }
+        cb(null, file);
+      })
+    )
+    .pipe(gulp.dest("src/assets/icons"));
+});
 
 gulp.task("html", function () {
   return gulp
@@ -14,5 +36,5 @@ gulp.task("html", function () {
 });
 
 gulp.task("watch", function () {
-  gulp.watch("src/html/**/*.html", gulp.series("html"));
+  gulp.watch("src/html/**/*.html", gulp.series("preprocess-svgs", "html"));
 });
