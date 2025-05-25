7b85dacd65b8585620500bf73f7e1fc28868f384
StuntHacks <me@stunthacks.eu>
2025-05-25 05:26:17 +0200

    Fix tab logic

--- src/ts/ui/elements/SystemTabElement.ts
+++ src/ts/ui/elements/SystemTabElement.ts
@@ -4,26 +4,35 @@ import { BigNumber } from "bignumber.js"
 
 export class SystemTabElement extends HTMLElement {
     private active: string;
-    private subTabs: HTMLDivElement;
 
     constructor() {
         super();
     }
 
     connectedCallback() {
-        this.subTabs = this.querySelector("nav.sub-tabs");
+        let subTabs = this.querySelector("nav.sub-tabs");
+        let background = this.querySelector(".tab-background");
+        console.log(this)
 
-        let tabs = this.subTabs.getElementsByTagName("span");
+        let tabs = subTabs.getElementsByTagName("span");
         for (let i = 0; i < tabs.length; i++) {
             tabs[i].addEventListener("click", (e: MouseEvent) => {
                 let target = (e.target as HTMLElement).closest("nav.sub-tabs span") as HTMLSpanElement;
-                let tab = this.querySelector(`section.tab[data-tab="${target.dataset.tab}"]`)
+                let tab = this.querySelector(`section.tab[data-tab="${target.dataset.tab}"]`);
                 if (target.classList.contains("active")) {
                     target.classList.remove("active");
                     tab.classList.remove("active");
+                    background.classList.remove("active");
                 } else {
+                    let tabs = this.querySelectorAll("section.tab");
+                    let tabHeaders = target.closest(".sub-tabs").querySelectorAll("span");
+
+                    tabs.forEach(e => e.classList.remove("active"));
+                    tabHeaders.forEach(e => e.classList.remove("active"));
+
                     target.classList.add("active");
                     tab.classList.add("active");
+                    background.classList.add("active");
                 }
             });
         }
