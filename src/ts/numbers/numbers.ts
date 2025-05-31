3f633dd434792d1e921b2115f04247cc95fc4170
StuntHacks <me@stunthacks.eu>
2025-05-31 07:41:39 +0200

    Fix formatting

--- src/ts/numbers/numbers.ts
+++ src/ts/numbers/numbers.ts
@@ -2,15 +2,16 @@ import { BigNumber } from "bignumber.js"
 
 export namespace Numbers {
     export const getFormatted = (num: BigNumber): string => {
-        if (num.toString().includes("e")) {
+        const fixed = num.toFixed(2);
+        if (fixed.includes("e")) {
             let ret;
 
-            let parts = num.toString().replace("+", "").split("e");
+            let parts = fixed.replace("+", "").split("e");
             parts[0] = parts[0].slice(0, 4);
 
             return parts.join("e");
         } else {
-            return num.toString();
+            return fixed;
         }
     }
     
