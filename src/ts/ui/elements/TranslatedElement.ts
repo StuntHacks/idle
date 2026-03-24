8772652462d08cc00e0705462d045b75a9102b37
stunthacks <me@stunthacks.eu>
2026-03-24 12:32:24 +0100

    Implement string interpolation

--- src/ts/ui/elements/TranslatedElement.ts
+++ src/ts/ui/elements/TranslatedElement.ts
@@ -11,6 +11,11 @@ export class TranslatedElement extends HTMLElement {
     public refresh(textId?: string) {
         this.textId = textId ?? this.textContent;
         let translated = Translator.getTranslation(this.textId);
+        const interpolations = JSON.parse(this.getAttribute("interpolate") || "[]");
+
+        for (let i = 0; i < interpolations.length; i++) {
+            translated = translated.replace(`$${i}`, interpolations[i]);
+        }
 
         if (translated) {
             this.innerHTML = translated;
