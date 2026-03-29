9582efcef1f3f17f16940dbf445ab134020c7e36
StuntHacks <me@stunthacks.eu>
2026-03-29 05:48:48 +0200

    Barebones migration stuff

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -3,18 +3,25 @@ import { initGame } from "./game_logic/Game";
 import { CustomElements } from "ui/CustomElements";
 
 export const main = async () => {
-    const game = initGame();
-
-    // initialize
-    CustomElements.initialize();
-
-    document.getElementById("magic-button").addEventListener("click", () => {
-        game.timeskip(3 * 3600);
-    });
-
-    // PopoverManager.add(new ExportPopover());
-
-    game.start();
-    UI.openSubTab("quantum-tab-energy");
-    document.addEventListener("contextmenu", (e) => e.preventDefault());
+    try {
+        const game = initGame();
+    
+        // initialize
+        CustomElements.initialize();
+    
+        document.getElementById("magic-button").addEventListener("click", () => {
+            game.timeskip(3 * 3600);
+        });
+    
+        // PopoverManager.add(new ExportPopover());
+    
+        game.start();
+        UI.openSubTab("quantum-tab-energy");
+        document.addEventListener("contextmenu", (e) => e.preventDefault());
+    } catch {
+        // todo: implement proper migration
+        localStorage.removeItem("idledynamics_saveFile");
+        localStorage.removeItem("idledynamics_settings");
+        location.reload();
+    }
 }
